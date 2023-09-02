d7b2671a87daed5020fcba9f7095a504e6d7a237
Khushboo Chauhan <khushboo84725@gmail.com>
2023-09-01 20:57:58 -0400

    footer padding issue fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-primary py-12 text-white w-full ">
+    <div className="bg-primary py-12 text-white w-full mt-12">
       <div className="container mx-auto flex items-center justify-center">
         Copyright &copy; FashionEcomm 2022 Inspired by Ecommerce Shop by
         Cristian Mihai
